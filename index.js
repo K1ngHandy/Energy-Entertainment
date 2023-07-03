64407e18b9fa477242f15db34b0545b733db8aef
K1ngHandy <a.king.life@pm.me>
2023-07-02 20:48:30 -0400

    accessing elements

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Declaring an array literal
 const concerts = ['April 26', 'April 29', 'May 5', 'May 10', 'May 17'];
+
+// Accessing elements in an array
+console.log('European Tour Dates!');
+concerts[0]; // April 26
+concerts[1]; // April 29
+concerts[2]; // May 5
+concerts[3]; // May 10
+concerts[4]; // May 17
